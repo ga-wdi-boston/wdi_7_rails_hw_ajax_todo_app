ed639665767b8aec57760230724882a9a1ab4c82
krk324 <krk32488@gmail.com>
2014-07-22 19:30:56 -0400

    Add js for todo list.

---
+++ app/assets/javascripts/TodoItem.js
@@ -0,0 +1,54 @@
+var TodoApp = {
+
+  tasks: [],
+
+  initialize: function(){
+    $('#task-form').on('submit',$.proxy(this.createTask, this));
+    $('#tasks-list').on('click', '#delete-button',$.proxy(this.deleteTask,this));
+    $('#tasks-list').on('click', '#done-button',$.proxy(this.doneTask, this));
+    $('#finished-list').on('click', '#delete-button',$.proxy(this.deleteTask,this));
+
+  },
+  createTask: function(event){
+    var task = $('#task-field').val();
+    if(task !== ''){
+      var newTask = new TodoItem(task);
+      this.tasks.push(newTask);
+      this.updateTasks();
+      $('#task-field').val('');
+    }
+    event.preventDefault();
+  },
+
+  deleteTask: function(event){
+    task = this.getTask(event);
+    this.tasks = this.tasks.filter(function(obj) {return obj.id !== task.id;});
+    this.updateTasks();
+    event.preventDefault();
+  },
+
+  doneTask: function(event){
+    task = this.getTask(event);
+    task.markCompleted();
+    this.updateTasks();
+    event.preventDefault();
+  },
+
+  getTask: function(event){
+    id = $(event.target).parent().data('id');
+    return $.grep(this.tasks,function(task){return task.id === id})[0];
+  },
+
+  updateTasks: function(event){
+    $('#tasks-list').empty();
+    $('#finished-list').empty();
+
+    this.tasks.forEach(function(task){
+      if(task.completedAt !== null) {
+        $('#finished-list').append(task.html(task));
+      } else {
+        $('#tasks-list').append(task.html(task));
+      }
+    });
+  }
+};
---
+++ app/assets/javascripts/app_ready.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  TodoApp.initialize();
+});
+
