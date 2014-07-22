f6cc255243e70c63dd869021a83aa811f32d3032
krk324 <krk32488@gmail.com>
2014-07-22 18:24:43 -0400

    Create rails api server

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+  def default_serializer_options
+    {root: false}
+  end
+
+  respond_to :json
+
+  def index
+
+    @tasks = Task.all
+    respond_with(@tasks)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    respond_with(@task)
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      respond_with(@task)
+    else
+      respond_with(@task.errors)
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:id, :title, :body)
+  end
+end
---
+++ db/migrate/20140722221443_tasks.rb
@@ -0,0 +1,9 @@
+class Tasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.text :task
+
+      t.timestamps
+    end
+  end
+end
